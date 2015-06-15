63b9f27615f1040fbc852594a877ca2b069597fb
BenSibley <ben@competethemes.com>
2015-06-15 08:04:28 -0400

    setup post content on blog

--- content-archive.php
+++ content-archive.php
@@ -1,15 +1,20 @@
 <div <?php post_class(); ?>>
 	<article>
 		<?php ct_chosen_featured_image(); ?>
-		<div class="post-meta">
-			<?php get_template_part('content/post-meta'); ?>
-		</div>
 		<div class='post-header'>
-			<h1 class='post-title'><?php the_title(); ?></h1>
-			<?php get_template_part('content/post-categories'); ?>
+			<h1 class='post-title'>
+				<a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
+			</h1>
+			<span class="post-date">
+				<?php
+				$date = date_i18n( get_option( 'date_format' ), strtotime( get_the_date('r') ) );
+				printf( __( 'Published %s', 'chosen' ), $date );
+				?>
+			</span>
 		</div>
 		<div class="post-content">
 			<?php ct_chosen_excerpt(); ?>
+			<?php get_template_part('content/comments-link'); ?>
 		</div>
 	</article>
 </div>
