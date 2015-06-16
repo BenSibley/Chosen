9da7252b4b6681728f60c209521bf12291b6eed8
BenSibley <ben@competethemes.com>
2015-06-16 08:49:37 -0400

    post content and styles

--- content.php
+++ content.php
@@ -1,20 +1,24 @@
 <div <?php post_class(); ?>>
 	<article>
 	    <?php ct_chosen_featured_image(); ?>
-	    <div class="post-meta">
-	        <?php get_template_part('content/post-meta'); ?>
-	    </div>
 	    <div class='post-header'>
 	        <h1 class='post-title'><?php the_title(); ?></h1>
-			<?php get_template_part('content/post-categories'); ?>
+		    <span class="post-date">
+				<?php
+				$date = date_i18n( get_option( 'date_format' ), strtotime( get_the_date('r') ) );
+				printf( __( 'Published %s', 'chosen' ), $date );
+				?>
+			</span>
 	    </div>
 	    <div class="post-content">
 	        <?php the_content(); ?>
 	        <?php wp_link_pages(array('before' => '<p class="singular-pagination">' . __('Pages:','chosen'), 'after' => '</p>', ) ); ?>
 	    </div>
-		<?php get_template_part('content/post-nav'); ?>
-		<?php get_template_part('content/post-author'); ?>
-	    <?php get_template_part('content/post-tags'); ?>
+		<div class="post-meta">
+			<?php get_template_part('content/post-categories'); ?>
+			<?php get_template_part('content/post-tags'); ?>
+			<?php get_template_part('content/post-nav'); ?>
+		</div>
 	</article>
 	<?php comments_template(); ?>
 </div>
