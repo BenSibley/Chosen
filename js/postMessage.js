159ea39b9ba509a9350728ac883f9141aebb2e8f
BenSibley <ben@competethemes.com>
2015-06-17 09:24:44 -0400

    tagline and site title now using postMessage

--- js/postMessage.js
+++ js/postMessage.js
@@ -4,5 +4,24 @@
      * Following functions are for utilizing the postMessage transport setting
      */
 
+    // Site title
+    wp.customize( 'blogname', function( value ) {
+        value.bind( function( to ) {
+            // if there is a logo, don't replace it
+            if( $('.site-title').find('img').length == 0 ) {
+                $( '.site-title a' ).text( to );
+            }
+        } );
+    } );
+    // Tagline
+    wp.customize( 'blogdescription', function( value ) {
+        value.bind( function( to ) {
+            var tagline = $('.tagline');
+            if( tagline.length == 0 ) {
+                $('#title-container').append('<p class="tagline"></p>');
+            }
+            tagline.text( to );
+        } );
+    } );
 
 } )( jQuery );
