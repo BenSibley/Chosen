c95d81ebb6e7c69507ccb84ae0e74ef05ba226f4
BenSibley <ben@competethemes.com>
2015-06-17 08:44:23 -0400

    updated archive header text domain

--- content/archive-header.php
+++ content/archive-header.php
@@ -2,7 +2,7 @@
 /* Category header */
 if( is_category() ){ ?>
 	<div class='archive-header'>
-		<i class="fa fa-folder-open" title="<?php _e('archive icon', 'founder'); ?>"></i>
+		<i class="fa fa-folder-open" title="<?php _e('archive icon', 'chosen'); ?>"></i>
 		<h2>
 			<?php _e('Category archive for:', 'chosen'); ?>
 			<?php single_cat_title(); ?>
@@ -13,7 +13,7 @@ if( is_category() ){ ?>
 /* Tag header */
 elseif( is_tag() ){ ?>
 	<div class='archive-header'>
-		<i class="fa fa-tag" title="<?php _e('tag icon', 'founder'); ?>"></i>
+		<i class="fa fa-tag" title="<?php _e('tag icon', 'chosen'); ?>"></i>
 		<h2>
 			<?php _e('Tag archive for:', 'chosen'); ?>
 			<?php single_tag_title(); ?>
@@ -25,7 +25,7 @@ elseif( is_tag() ){ ?>
 elseif( is_author() ){
 	$author = get_userdata(get_query_var('author')); ?>
 	<div class='archive-header'>
-		<i class="fa fa-user" title="<?php _e('author icon', 'founder'); ?>"></i>
+		<i class="fa fa-user" title="<?php _e('author icon', 'chosen'); ?>"></i>
 		<h2>
 			<?php _e('Author archive for:', 'chosen'); ?>
 			<?php echo $author->nickname; ?>
@@ -36,7 +36,7 @@ elseif( is_author() ){
 /* Date header */
 elseif( is_date() ){ ?>
 	<div class='archive-header'>
-		<i class="fa fa-calendar" title="<?php _e('calendar icon', 'founder'); ?>"></i>
+		<i class="fa fa-calendar" title="<?php _e('calendar icon', 'chosen'); ?>"></i>
 		<h2>
 			<?php _e('Date archive for:', 'chosen'); ?>
 			<?php single_month_title(' '); ?>
