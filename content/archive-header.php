a5804ead5ca02a55fce9edf5ed8050db8b0ed94b
BenSibley <ben@competethemes.com>
2015-12-17 20:18:41 -0500

    archive header simplified with core functions

--- content/archive-header.php
+++ content/archive-header.php
@@ -1,45 +1,24 @@
 <?php
-/* Category header */
-if( is_category() ){ ?>
-	<div class='archive-header'>
-		<h2>
-			<i class="fa fa-folder-open" title="<?php _e('archive icon', 'chosen'); ?>"></i>
-			<?php printf( __('You are viewing the <span>%s</span> category archive.', 'chosen'), single_cat_title('', false) ); ?>
-		</h2>
-		<?php if ( category_description() ) echo category_description(); ?>
-	</div>
-<?php
+
+if ( ! is_archive() ) {
+	return;
 }
-/* Tag header */
-elseif( is_tag() ){ ?>
-	<div class='archive-header'>
-		<h2>
-			<i class="fa fa-tag" title="<?php _e('tag icon', 'chosen'); ?>"></i>
-			<?php printf( __('You are viewing the <span>%s</span> tag archive.', 'chosen'), single_tag_title('', false) ); ?>
-		</h2>
-		<?php if ( tag_description() ) echo tag_description(); ?>
-	</div>
-<?php
+
+$icon_class = 'folder-open';
+
+if ( is_tag() ) {
+	$icon_class = 'tag';
+} elseif ( is_author() ) {
+	$icon_class = 'user';
+} elseif ( is_date() ) {
+	$icon_class = 'calendar';
 }
-/* Author header */
-elseif( is_author() ){
-	$author = get_userdata(get_query_var('author')); ?>
-	<div class='archive-header'>
-		<h2>
-			<i class="fa fa-user" title="<?php _e('author icon', 'chosen'); ?>"></i>
-			<?php printf( __("You are viewing <span>%s</span>'s post archive.", "chosen"), $author->nickname ); ?>
-		</h2>
-		<?php if ( get_the_author_meta( 'description' ) ) echo '<p>' . get_the_author_meta( 'description' ) . '</p>'; ?>
-	</div>
-<?php
-}
-/* Date header */
-elseif( is_date() ){ ?>
-	<div class='archive-header'>
-		<h2>
-			<i class="fa fa-calendar" title="<?php _e('calendar icon', 'chosen'); ?>"></i>
-			<?php printf( __('You are viewing the date archive for <span>%s</span>.', 'chosen'), single_month_title('', false) ); ?>
-		</h2>
-	</div>
-<?php
-}
+?>
+
+<div class='archive-header'>
+	<h2>
+		<i class="fa fa-<?php echo $icon_class; ?>"></i>
+		<?php the_archive_title(); ?>
+	</h2>
+	<?php the_archive_description(); ?>
+</div>
