c4287a7588f01030870eb6a9546f205ad4bf44aa
BenSibley <ben@competethemes.com>
2015-12-17 20:26:11 -0500

    removed unused multi checkbox control

--- inc/customizer.php
+++ inc/customizer.php
@@ -22,29 +22,6 @@ function ct_chosen_add_customizer_content( $wp_customize ) {
 	
 	/***** Add Custom Controls *****/
 
-	// create multi-checkbox/select control
-	class ct_chosen_multi_checkbox_control extends WP_Customize_Control {
-		public $type = 'multi-checkbox';
-
-		public function render_content() {
-
-			if ( empty( $this->choices ) )
-				return;
-			?>
-			<label>
-				<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-				<select id="comment-display-control" <?php $this->link(); ?> multiple="multiple" style="height: 100%;">
-					<?php
-					foreach ( $this->choices as $value => $label ) {
-						$selected = ( in_array( $value, $this->value() ) ) ? selected( 1, 1, false ) : '';
-						echo '<option value="' . esc_attr( $value ) . '"' . $selected . '>' . $label . '</option>';
-					}
-					?>
-				</select>
-			</label>
-		<?php }
-	}
-
 	// create textarea control
 	class ct_chosen_textarea_control extends WP_Customize_Control {
 		public $type = 'textarea';
