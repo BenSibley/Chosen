60be5d644edc821636c1f4091ddeb09828797a5f
BenSibley <ben@competethemes.com>
2016-01-18 09:19:14 -0500

    no longer showing last post on page 1 again on page 2

--- functions.php
+++ functions.php
@@ -548,14 +548,26 @@ function ct_chosen_get_content_template() {
 }
 
 // prevent odd number of posts on page 2+ of blog if extra-wide post used
-function ct_chosen_adjust_post_count( $query ) {
+function ct_chosen_adjust_post_count( $query) {
+
+	global $wp_query;
 
 	$extra_wide = get_theme_mod( 'full_width_post' );
 
 	if ( $extra_wide != 'no' ) {
 
 		if ( $query->is_home() && $query->is_main_query() && $query->is_paged() ) {
-			$query->set( 'posts_per_page', get_option( 'posts_per_page' ) - 1 );
+
+			$posts_per_page = get_option('posts_per_page');
+
+			// get number of previous posts
+			$offset = ( $query->query_vars['paged'] - 1 ) * $posts_per_page;
+
+			// offset post count plus one for every page after pag 2
+			$query->set( 'offset', $offset - ( $query->query_vars['paged'] - 2 ) );
+
+			// drop the posts per page by 1
+			$query->set( 'posts_per_page', $posts_per_page - 1 );
 		}
 	}
 }
