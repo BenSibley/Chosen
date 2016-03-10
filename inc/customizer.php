258286055467ed3f9176aeb3e02e476f29b785c0
BenSibley <ben@competethemes.com>
2016-03-10 15:30:09 -0500

    updated Customizer ad

--- inc/customizer.php
+++ inc/customizer.php
@@ -315,7 +315,7 @@ function ct_chosen_sanitize_css( $css ) {
 
 function ct_chosen_customize_preview_js() {
 
-	$content = "<script>jQuery('#customize-info').prepend('<div class=\"upgrades-ad\"><a href=\"https://www.competethemes.com/chosen-pro/\" target=\"_blank\">" . __( 'View the Chosen Pro Plugin', 'chosen' ) . " <span>&rarr;</span></a></div>')</script>";
+	$content = "<script>jQuery('#customize-info').append('<div class=\"upgrades-ad\"><span>Add more features with <a href=\"https://www.competethemes.com/chosen-pro/\" target=\"_blank\">" . __( 'Chosen Pro', 'chosen' ) . " <span>&rarr;</span></a></span></div>')</script>";
 	echo apply_filters( 'ct_chosen_customizer_ad', $content );
 }
 
