8297de425e2f6f46dd078fc70d4e853a29eda550
BenSibley <ben@competethemes.com>
2016-12-01 17:57:40 -0500

    added redirect to dashboard upon activation

--- functions.php
+++ functions.php
@@ -643,3 +643,16 @@ if ( ! function_exists( 'ct_chosen_allow_skype_protocol' ) ) {
 	}
 }
 add_filter( 'kses_allowed_protocols' , 'ct_chosen_allow_skype_protocol' );
+
+// trigger theme switch on link click and send to Appearance menu
+function ct_chosen_welcome_redirect() {
+
+	$welcome_url = add_query_arg(
+		array(
+			'page' => 'chosen-options'
+		),
+		admin_url( 'themes.php' )
+	);
+	wp_redirect( esc_url( $welcome_url ) );
+}
+add_action( 'after_switch_theme', 'ct_chosen_welcome_redirect' );
