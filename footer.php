fabb2478b211dc8d828f91020d3789e2c4275278
Ben Sibley <ben@competethemes.com>
2020-12-23 11:45:42 -0500

    added nofollow to footer link

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@ if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_
 	<div class="design-credit">
         <span>
             <?php
-            $footer_text = sprintf( __( '<a href="%1$s">%2$s WordPress Theme</a> by Compete Themes.', 'chosen' ), 'https://www.competethemes.com/chosen/', wp_get_theme( get_template() ) );
+            $footer_text = sprintf( __( '<a href="%1$s" rel="nofollow">%2$s WordPress Theme</a> by Compete Themes.', 'chosen' ), 'https://www.competethemes.com/chosen/', wp_get_theme( get_template() ) );
             $footer_text = apply_filters( 'ct_chosen_footer_text', $footer_text );
             echo do_shortcode( wp_kses_post( $footer_text ) );
             ?>
